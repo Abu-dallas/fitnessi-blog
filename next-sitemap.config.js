1bf13a08d4b2f85c8896a3473e1f368de323bfa2
Abu Dallas <gyakubu14@gmail.com>
2025-07-01 12:08:02 +0100

    updated sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://fitnessi.top", // your live domain
+  generateRobotsTxt: true, // create robots.txt
+  sitemapSize: 7000,
+};
